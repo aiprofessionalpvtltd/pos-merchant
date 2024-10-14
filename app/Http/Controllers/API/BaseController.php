057a1c70b5e838b662f19a8d0aa338a07244a3e0
Salman Mehboob <appflex.developer@gmail.com>
2024-10-14 12:17:08 +0500

    add model

--- app/Http/Controllers/API/BaseController.php
+++ app/Http/Controllers/API/BaseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\ApiLog;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
